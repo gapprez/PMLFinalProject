f15e211b4c88ec740c044e0401e87209b2ce6d8d
tossenxD <thorbjorn.bb@gmail.com>
2024-01-14 17:16:51 +0100

    observations

---
+++ gp/gp.py
@@ -0,0 +1,15 @@
+## The pyro GP tutorial as starting point:
+## https://pyro.ai/examples/gp.html
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import pyro
+import pyro.contrib.gp as gp
+import pyro.distributions as dist
+
+# Partition observations
+X = np.asarray([x / 29 for x in range(1, 31)])
+np.random.shuffle(X)
+Y = 6 * np.square(X) - np.square(np.sin(6 * np.pi * X)) - 5 * np.power(X, 4) + 3 / 2 + np.random.normal(0.0, 0.1, 30)
+Xtrain, Xtest, Ytrain, Ytest = X[10:], X[:10], Y[10:], Y[:10]
